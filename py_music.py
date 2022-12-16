bc46739bc7a6a264fa824d418feecc52b14a96d6
YoakamWang <90314896+YoakamWang@users.noreply.github.com>
2022-12-16 09:56:05 +0800

    Add files via upload

--- py_music.py
+++ py_music.py
@@ -20,13 +20,42 @@ def cut_mp3(filepath):
     out_music = music[30000: 229000]
 
     # 导出
-    out_music.export(out_f="./蜗牛-周杰伦.mp3", format='mp3')   # 可以指定bitrate为64k比特率 None为源文件
+    out_music.export(out_f="./Lemon_Tree01.mp3", format='mp3')   # 可以指定bitrate为64k比特率 None为源文件
 
     print('done')
     pass
+'''
+@Time    : 2020/2/12 14:48
+@FileName: joinVoice.py
+@Author  : Yoakam
+@Email   : yuwang@nilfisk.com
+'''
+
+def joinVoice():
+    file1_name = r"../data/sound1.mp3"
+    file2_name = r"../data/sound2.mp3"
+    # 加载需要拼接的两个文件
+    sound1 = AudioSegment.from_mp3(file1_name)
+    sound2 = AudioSegment.from_mp3(file2_name)
+    # 取得两个文件的声音分贝
+    db1 = sound1.dBFS
+    db2 = sound2.dBFS
+    dbplus = db1 - db2
+    # 声音大小
+    if dbplus < 0:
+        sound1 += abs(dbplus)
+    else:
+        sound2 += abs(dbplus)
+    # 拼接两个音频文件
+    finSound = sound1 + sound2
+    save_name = r"../data/" + "finSound" + file1_name[-4:]
+    print("save_path:", save_name)
+
+    finSound.export(save_name, format="mp3", tags={'artist': 'AppLeU0', 'album': save_name[:-4]})
+    return True
 
 
 if __name__ == '__main__':
-    src_path = r'./周杰伦-蜗牛_need_cut.mp3'   # seconds: 30 -> 3'49(229)
+    src_path = r'./Lemon_Tree.mp3'   # seconds: 30 -> 3'49(229)
     cut_mp3(filepath=src_path)
 
