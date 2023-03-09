bd68de6f6a3ca0fc43ec91384f771bec67e1ea75
YoakamWang <90314896+YoakamWang@users.noreply.github.com>
2023-03-09 10:07:46 +0800

    Add files via upload

--- Daily_practise.py
+++ Daily_practise.py
@@ -20,3 +20,21 @@ while guess != rd and guess != "exit":
         print("Right!")
         print("You took only", c, "tries!")
 input()
+
+def fibonacci():
+    num = int(input("How many numbers that generates?:"))
+    i = 1
+    if num == 0:
+        fib = []
+    elif num == 1:
+        fib = [1]
+    elif num == 2:
+        fib = [1,1]
+    elif num > 2:
+        fib = [1,1]
+        while i < (num - 1):
+            fib.append(fib[i] + fib[i-1])
+            i += 1
+    return fib
+print(fibonacci())
+input()
