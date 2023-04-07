350c4ffcfc4864e6c69a580675d25284947d75ba
YoakamWang <90314896+YoakamWang@users.noreply.github.com>
2023-04-07 13:42:53 +0800

    Add files via upload

---
+++ get_data_array.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+
+def data_file():
+    original_data = [(1.2358771562576294, 0.8885444402694702), (6.086360454559326, 1.0427088737487793),
+                     (5.936509132385254, 4.022181510925293), (1.1873016357421875, 3.7481889724731445),
+                     (1.2530081272125244, 0.9054474234580994), (1.2584795951843262, 0.8969888091087341)]
+    pdo_data = pd.DataFrame(original_data)
+    with open("abc.txt", "w+") as f1:
+        pdo_data.to_csv(f1, index=None, line_terminator="\n")
+
+
+def file_data():
+    with open("abc.txt", "r") as f2:
+        get_data = pd.read_csv(f2)
+        final_data = get_data.to_numpy()
+        print(final_data)
+
+
+if __name__ == "__main__":
+    file_data()
